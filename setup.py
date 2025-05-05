785099e91738097dc18d04a9c7393bbc10aa8b90
Pablo <pablorangel@gmail.com>
2025-05-05 10:13:05 -0300

    Example

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_module',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Pablo',
+    description='Exemplo de projeto Python com CI/CD',
+    python_requires='>=3.7',
+)
